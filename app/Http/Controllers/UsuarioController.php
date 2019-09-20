bab75fa0350e1ae4fe9d36e73281002b882a3e5a
JoseXD <jose_30_escorpio@hotmail.com>
2019-09-20 01:56:33 -0500

    busqueda en usuarioController supermegaarchirecontradesarrollada por EasyCode

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -14,29 +14,21 @@ class UsuarioController extends Controller
         $texto = $request->texto;
         $items_per_page = 4;
 
-        if ( $estado == 2 && $texto == '' ) {
-            $usuarios = Usuario::join('persona', 'usuario.persona_id', '=', 'persona.id')->join('rol', 'usuario.rol_id', '=', 'rol.id')
-                               ->select('persona.nombre', 'persona.direccion', 'persona.created_at as fecha_creacion', 'persona.updated_at as fecha_actualizacion', 'persona.deleted_at as fecha_eliminacion',
-                                        'usuario.id', 'usuario.estado', 'rol.nombre as rol')
-                               ->orderBy('rol', 'asc')->paginate($items_per_page);
-        } else if ( $texto == ''){
-            $usuarios = Usuario::join('persona', 'usuario.persona_id', '=', 'persona.id')->join('rol', 'usuario.rol_id', '=', 'rol.id')
-                               ->select('persona.nombre', 'persona.direccion', 'persona.created_at as fecha_creacion', 'persona.updated_at as fecha_actualizacion', 'persona.deleted_at as fecha_eliminacion',
-                                        'usuario.id', 'usuario.estado', 'rol.nombre as rol')
-                               ->where('usuario.estado', '=', $estado)
-                               ->orderBy('rol', 'asc')->paginate($items_per_page);
-                               // where estado.id = A and ( persona.nombre like %% or rol like %% )
-        } else {
-            $usuarios = Usuario::join('persona', 'usuario.persona_id', '=', 'persona.id')->join('rol', 'usuario.rol_id', '=', 'rol.id')
-                               ->select('persona.nombre', 'persona.direccion', 'persona.created_at as fecha_creacion', 'persona.updated_at as fecha_actualizacion', 'persona.deleted_at as fecha_eliminacion',
-                                        'usuario.id', 'usuario.estado', 'rol.nombre as rol')
-                               ->where('usuario.estado', '=', $estado)
-                               ->where(function ($query) {
-                                    $query->where('persona.nombre', 'like', '%'.$texto.'%');
-                                        //   ->orWhere('rol', 'like', '%'.$texto.'%');
-                               })
-                               ->orderBy('rol', 'asc')->paginate($items_per_page);
-        }
+        $usuarios = Usuario::join('persona', 'usuario.persona_id', '=', 'persona.id')->join('rol', 'usuario.rol_id', '=', 'rol.id')
+                            ->select('persona.nombre', 'persona.direccion', 'persona.created_at as fecha_creacion', 'persona.updated_at as fecha_actualizacion', 'persona.deleted_at as fecha_eliminacion',
+                                    'usuario.id', 'usuario.estado', 'rol.nombre as rol')
+                            ->where(function ($query) use ($estado) {
+                                if ( $estado != 2 ) {
+                                    $query->where('usuario.estado', '=', $estado);
+                                }
+                            })
+                            ->where(function ($query) use ($texto) {
+                                if ( $texto != '' ) {
+                                    $query->where('rol.nombre', 'like', '%'.$texto.'%')
+                                        ->orWhere('persona.nombre', 'like', '%'.$texto.'%');
+                                }
+                            })
+                            ->orderBy('rol', 'asc')->paginate($items_per_page);
 
         return [
             'paginacion' => [
