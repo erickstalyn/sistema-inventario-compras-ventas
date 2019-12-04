0df87107b0a0c07c63c7f2a34e623a2a980f3955
Stalyn <espacherrezpuyen@gmail.com>
2019-12-04 16:35:52 -0500

    Antes de modificar productos

---
+++ database/seeds/Detalle_producto.php
@@ -0,0 +1,89 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class Detalle_producto extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // //ALMACEN 1
+        // DB::table('detalle_producto')->insert(array(//1
+        //     'substock' => 50,
+        //     'precio_menor' => 65,
+        //     'precio_mayor' => 55,
+        //     'centro_id' => 4,
+        //     'producto_id' => 1
+        // ));
+        // DB::table('detalle_producto')->insert(array(//1
+        //     'substock' => 100,
+        //     'precio_menor' => 65,
+        //     'precio_mayor' => 55,
+        //     'centro_id' => 4,
+        //     'producto_id' => 2
+        // ));
+        // DB::table('detalle_producto')->insert(array(//1
+        //     'substock' => 150,
+        //     'precio_menor' => 45,
+        //     'precio_mayor' => 40,
+        //     'centro_id' => 4,
+        //     'producto_id' => 3
+        // ));
+        // DB::table('detalle_producto')->insert(array(//1
+        //     'substock' => 124,
+        //     'precio_menor' => 45,
+        //     'precio_mayor' => 40,
+        //     'centro_id' => 4,
+        //     'producto_id' => 4
+        // ));
+        // DB::table('detalle_producto')->insert(array(//1
+        //     'substock' => 269,
+        //     'precio_menor' => 45,
+        //     'precio_mayor' => 40,
+        //     'centro_id' => 4,
+        //     'producto_id' => 5
+        // ));
+
+        // //ALMACEN 2
+        // DB::table('detalle_producto')->insert(array(//1
+        //     'substock' => 50,
+        //     'precio_menor' => 65,
+        //     'precio_mayor' => 55,
+        //     'centro_id' => 5,
+        //     'producto_id' => 1
+        // ));
+        // DB::table('detalle_producto')->insert(array(//1
+        //     'substock' => 100,
+        //     'precio_menor' => 65,
+        //     'precio_mayor' => 55,
+        //     'centro_id' => 5,
+        //     'producto_id' => 2
+        // ));
+        // DB::table('detalle_producto')->insert(array(//1
+        //     'substock' => 150,
+        //     'precio_menor' => 45,
+        //     'precio_mayor' => 40,
+        //     'centro_id' => 5,
+        //     'producto_id' => 3
+        // ));
+        // DB::table('detalle_producto')->insert(array(//1
+        //     'substock' => 124,
+        //     'precio_menor' => 45,
+        //     'precio_mayor' => 40,
+        //     'centro_id' => 5,
+        //     'producto_id' => 4
+        // ));
+        // DB::table('detalle_producto')->insert(array(//1
+        //     'substock' => 269,
+        //     'precio_menor' => 45,
+        //     'precio_mayor' => 40,
+        //     'centro_id' => 5,
+        //     'producto_id' => 5
+        // ));
+
+    }
+}
--- database/seeds/ProductoSeeder.php
+++ database/seeds/ProductoSeeder.php
@@ -14,130 +14,133 @@ class ProductoSeeder extends Seeder
     {
         $now = Carbon::now('America/Lima')->toDateString();
         //Campera Silmar R3D
-        DB::table('producto')->insert(array(//1
-            'superproducto_id' => 1,
-            'codigo' => '1234567890123',
-            'nombre' => 'Campera SILMAR R3D Grande Rojo',
-            'descripcion' => 'Ideal para viajeros extranjeros',
-            'size' => 'Grande',
-            'color' => 'Rojo',
-            'costo_produccion' => 31.50,
-            'precio_menor' => 65,
-            'precio_mayor' => 55,
-            'created_at' => $now
-        ));
-        DB::table('producto')->insert(array(//2
-            'superproducto_id' => 1,
-            'codigo' => '1614678965456',
-            'nombre' => 'Campera SILMAR R3D Grande Amarillo',
-            'descripcion' => 'Ideal para viajeros extranjeros',
-            'size' => 'Grande',
-            'color' => 'Amarillo',
-            'costo_produccion' => 31.50,
-            'precio_menor' => 65,
-            'precio_mayor' => 55,
-            'created_at' => $now
-        ));
-        DB::table('producto')->insert(array(//3
-            'superproducto_id' => 1,
-            'codigo' => '8769874534563',
-            'nombre' => 'Campera SILMAR R3D Grande Azul',
-            'descripcion' => 'Ideal para viajeros extranjeros',
-            'size' => 'Grande',
-            'color' => 'Azul',
-            'costo_produccion' => 31.50,
-            'precio_menor' => 65,
-            'precio_mayor' => 55,
-            'created_at' => $now
-        ));
-        DB::table('producto')->insert(array(//4
-            'superproducto_id' => 1,
-            'codigo' => '7978560982345',
-            'nombre' => 'Campera SILMAR R3D Mediano Negro',
-            'descripcion' => 'Ideal para viajeros extranjeros',
-            'size' => 'Mediano',
-            'color' => 'Negro',
-            'costo_produccion' => 31.50,
-            'precio_menor' => 65,
-            'precio_mayor' => 55,
-            'created_at' => $now
-        ));
-        DB::table('producto')->insert(array(//5
-            'superproducto_id' => 1,
-            'codigo' => '7978560982345',
-            'nombre' => 'Campera SILMAR R3D Mediano Marron',
-            'descripcion' => 'Ideal para viajeros extranjeros',
-            'size' => 'Mediano',
-            'color' => 'Marron',
-            'costo_produccion' => 31.50,
-            'precio_menor' => 65,
-            'precio_mayor' => 55,
-            'created_at' => $now
-        ));
-        DB::table('producto')->insert(array(//6
-            'superproducto_id' => 1,
-            'codigo' => '7978560982345',
-            'nombre' => 'Campera SILMAR R3D Mediana Celeste',
-            'descripcion' => 'Ideal para viajeros extranjeros',
-            'size' => 'Mediano',
-            'color' => 'Celesto',
-            'costo_produccion' => 31.50,
-            'precio_menor' => 65,
-            'precio_mayor' => 55,
-            'created_at' => $now
-        ));
+        // DB::table('producto')->insert(array(//1
+        //     'superproducto_id' => 1,
+        //     'codigo' => '1234567890123',
+        //     'nombre' => 'Campera SILMAR R3D Grande Rojo',
+        //     'descripcion' => 'Ideal para viajeros extranjeros',
+        //     'size' => 'Grande',
+        //     'color' => 'Rojo',
+        //     'costo_produccion' => 31.50,
+        //     'precio_menor' => 65,
+        //     'precio_mayor' => 55,
+        //     'created_at' => $now
+        // ));
+        // DB::table('producto')->insert(array(//2
+        //     'superproducto_id' => 1,
+        //     'codigo' => '1614678965456',
+        //     'nombre' => 'Campera SILMAR R3D Grande Amarillo',
+        //     'descripcion' => 'Ideal para viajeros extranjeros',
+        //     'size' => 'Grande',
+        //     'color' => 'Amarillo',
+        //     'costo_produccion' => 31.50,
+        //     'precio_menor' => 65,
+        //     'precio_mayor' => 55,
+        //     'created_at' => $now
+        // ));
+        // DB::table('producto')->insert(array(//3
+        //     'superproducto_id' => 1,
+        //     'codigo' => '8769874534563',
+        //     'nombre' => 'Campera SILMAR R3D Grande Azul',
+        //     'descripcion' => 'Ideal para viajeros extranjeros',
+        //     'size' => 'Grande',
+        //     'color' => 'Azul',
+        //     'costo_produccion' => 31.50,
+        //     'precio_menor' => 65,
+        //     'precio_mayor' => 55,
+        //     'created_at' => $now
+        // ));
+        // DB::table('producto')->insert(array(//4
+        //     'superproducto_id' => 1,
+        //     'codigo' => '7978560982345',
+        //     'nombre' => 'Campera SILMAR R3D Mediano Negro',
+        //     'descripcion' => 'Ideal para viajeros extranjeros',
+        //     'size' => 'Mediano',
+        //     'color' => 'Negro',
+        //     'costo_produccion' => 31.50,
+        //     'precio_menor' => 65,
+        //     'precio_mayor' => 55,
+        //     'created_at' => $now
+        // ));
+        // DB::table('producto')->insert(array(//5
+        //     'superproducto_id' => 1,
+        //     'codigo' => '7978560982345',
+        //     'nombre' => 'Campera SILMAR R3D Mediano Marron',
+        //     'descripcion' => 'Ideal para viajeros extranjeros',
+        //     'size' => 'Mediano',
+        //     'color' => 'Marron',
+        //     'costo_produccion' => 31.50,
+        //     'precio_menor' => 65,
+        //     'precio_mayor' => 55,
+        //     'created_at' => $now
+        // ));
 
-        //Mochila Porta XT
-        DB::table('producto')->insert(array(//7
-            'superproducto_id' => 2,
-            'codigo' => '7978560982345',
-            'nombre' => 'Mochila Porta XT Pequeña Negro',
-            'descripcion' => 'Perfecto para escaloneros',
-            'size' => 'Pequeño',
-            'color' => 'Negro',
-            'costo_produccion' => 25.40,
-            'precio_menor' => 40.00,
-            'precio_mayor' => 35.60,
-            'created_at' => $now
-        ));
-        DB::table('producto')->insert(array(//8
-            'superproducto_id' => 2,
-            'codigo' => '7978560982345',
-            'nombre' => 'Mochila Porta XT Pequeña Azul',
-            'descripcion' => 'Perfecto para escaloneros',
-            'size' => 'Pequeño',
-            'color' => 'Azul',
-            'costo_produccion' => 25.40,
-            'precio_menor' => 40.00,
-            'precio_mayor' => 35.60,
-            'created_at' => $now
-        ));
-        DB::table('producto')->insert(array(//9
-            'superproducto_id' => 2,
-            'codigo' => '7978560982345',
-            'nombre' => 'Mochila Porta XT Grande Roja',
-            'descripcion' => 'Perfecto para escaloneros',
-            'size' => 'Grande',
-            'color' => 'Rojo',
-            'costo_produccion' => 25.40,
-            'precio_menor' => 40.00,
-            'precio_mayor' => 35.60,
-            'created_at' => $now
-        ));
 
-        //Cartera Azalea YUR
-        DB::table('producto')->insert(array(//10
-            'superproducto_id' => 3,
-            'codigo' => '7978560982345',
-            'nombre' => 'Cartera Azalea YUR Grande Rojo',
-            'descripcion' => 'Mochila con mucho espacio',
-            'size' => 'Grande',
-            'color' => 'Rojo',
-            'costo_produccion' => 47.60,
-            'precio_menor' => 58.90,
-            'precio_mayor' => 50.30,
-            'created_at' => $now
-        ));
+
+        // DB::table('producto')->insert(array(//6
+        //     'superproducto_id' => 1,
+        //     'codigo' => '7978560982345',
+        //     'nombre' => 'Campera SILMAR R3D Mediana Celeste',
+        //     'descripcion' => 'Ideal para viajeros extranjeros',
+        //     'size' => 'Mediano',
+        //     'color' => 'Celeste',
+        //     'costo_produccion' => 31.50,
+        //     'precio_menor' => 65,
+        //     'precio_mayor' => 55,
+        //     'created_at' => $now
+        // ));
+
+        // //Mochila Porta XT
+        // DB::table('producto')->insert(array(//7
+        //     'superproducto_id' => 2,
+        //     'codigo' => '7978560982345',
+        //     'nombre' => 'Mochila Porta XT Pequeña Negro',
+        //     'descripcion' => 'Perfecto para escaloneros',
+        //     'size' => 'Pequeño',
+        //     'color' => 'Negro',
+        //     'costo_produccion' => 25.40,
+        //     'precio_menor' => 40.00,
+        //     'precio_mayor' => 35.60,
+        //     'created_at' => $now
+        // ));
+        // DB::table('producto')->insert(array(//8
+        //     'superproducto_id' => 2,
+        //     'codigo' => '7978560982345',
+        //     'nombre' => 'Mochila Porta XT Pequeña Azul',
+        //     'descripcion' => 'Perfecto para escaloneros',
+        //     'size' => 'Pequeño',
+        //     'color' => 'Azul',
+        //     'costo_produccion' => 25.40,
+        //     'precio_menor' => 40.00,
+        //     'precio_mayor' => 35.60,
+        //     'created_at' => $now
+        // ));
+        // DB::table('producto')->insert(array(//9
+        //     'superproducto_id' => 2,
+        //     'codigo' => '7978560982345',
+        //     'nombre' => 'Mochila Porta XT Grande Roja',
+        //     'descripcion' => 'Perfecto para escaloneros',
+        //     'size' => 'Grande',
+        //     'color' => 'Rojo',
+        //     'costo_produccion' => 25.40,
+        //     'precio_menor' => 40.00,
+        //     'precio_mayor' => 35.60,
+        //     'created_at' => $now
+        // ));
+
+        // //Cartera Azalea YUR
+        // DB::table('producto')->insert(array(//10
+        //     'superproducto_id' => 3,
+        //     'codigo' => '7978560982345',
+        //     'nombre' => 'Cartera Azalea YUR Grande Rojo',
+        //     'descripcion' => 'Mochila con mucho espacio',
+        //     'size' => 'Grande',
+        //     'color' => 'Rojo',
+        //     'costo_produccion' => 47.60,
+        //     'precio_menor' => 58.90,
+        //     'precio_mayor' => 50.30,
+        //     'created_at' => $now
+        // ));
 
     }
 }
