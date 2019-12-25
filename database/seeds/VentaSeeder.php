a476f8cefcdf600585351ff726aee1c717e0f54e
JoseXD <jose_30_escorpio@hotmail.com>
2019-12-25 12:40:57 -0500

    Problema: valores de codigo de venta en los seeders muy largos. Estado: reparado

--- database/seeds/VentaSeeder.php
+++ database/seeds/VentaSeeder.php
@@ -17,7 +17,7 @@ class VentaSeeder extends Seeder
         DB::table('venta')->insert(array(
             'cliente_id' => NULL,
             'centro_id' => 1,
-            'codigo' => '1234567891234567',
+            'codigo' => '123456789123456',
             'total' => 100,
             'total_faltante' => NULL,
             'tipo' => 11,
@@ -26,7 +26,7 @@ class VentaSeeder extends Seeder
         DB::table('venta')->insert(array(
             'cliente_id' => 1,
             'centro_id' => 1,
-            'codigo' => '3641523498521123',
+            'codigo' => '364152349852113',
             'total' => 200,
             'total_faltante' => NULL,
             'tipo' => 11,
@@ -35,7 +35,7 @@ class VentaSeeder extends Seeder
         DB::table('venta')->insert(array(
             'cliente_id' => 5,
             'centro_id' => 1,
-            'codigo' => '6714983264824532',
+            'codigo' => '671498326482432',
             'total' => 300,
             'total_faltante' => 300,
             'tipo' => 21,
@@ -44,7 +44,7 @@ class VentaSeeder extends Seeder
         DB::table('venta')->insert(array(
             'cliente_id' => 6,
             'centro_id' => 1,
-            'codigo' => '13627985642853624',
+            'codigo' => '136279856428364',
             'total' => 400,
             'total_faltante' => 400,
             'tipo' => 21,
