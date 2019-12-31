6d6278cf495177ac4ec371baec402a9249e658ba
Stalyn <espacherrezpuyen@gmail.com>
2019-12-30 22:37:45 -0500

    Grafico mejorado, pero sigue siendo grafico de barras

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -10,12 +10,7 @@ class DashboardController extends Controller
     public function getEVentas(Request $request){
 
         $anio = date('Y');
-        // $ingresos = DB::table('ingresos as i')->select(DB::raw('MONTH(i.fecha_hora) as mes'), DB::raw('YEAR(i.fecha_hora) as anio'), DB::raw('SUM(i.total) as total'))
-        //             ->whereYear('i.fecha_hora', $anio)
-        //             ->groupBy(DB::raw('MONTH(i.fecha_hora)'), DB::raw('YEAR(i.fecha_hora)'))
-        //             ->get();
-
-        $ventas = DB::table('venta as v')->select(DB::raw('MONTH(v.created_at) as mes'), DB::raw('YEAR(v.created_at) as anvo'), DB::raw('COUNT(*) as num_ventas'))
+        $ventas = DB::table('venta as v')->select(DB::raw('MONTH(v.created_at) as mes'), DB::raw('YEAR(v.created_at) as year'), DB::raw('COUNT(*) as num_ventas'))
                     ->whereYear('v.created_at', $anio)
                     ->groupBy(DB::raw('MONTH(v.created_at)'), DB::raw('YEAR(v.created_at)'))
                     ->get();
--- resources/js/components/Dashboard.vue
+++ resources/js/components/Dashboard.vue
@@ -9,26 +9,10 @@
                 </div>
                 <div class="card-body">
                     <div class="row">
-                        <div class="col-md-6">
+                        <div class="col-md-12">
                             <div class="card card-chart">
                                 <div class="card-header">
-                                    <h4>Ingresos</h4>
-                                </div>
-                                <div class="card-content">
-                                    <div class="ct-chart">
-                                        <canvas id="ingresos">
-                                        </canvas>
-                                    </div>
-                                </div>
-                                <div class="card-footer">
-                                    <p>Compras de los últimos meses.</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-6">
-                            <div class="card card-chart">
-                                <div class="card-header">
-                                    <h4>Ventas</h4>
+                                    <h4>Número de ventas mensuales</h4>
                                 </div>
                                 <div class="card-content">
                                     <div class="ct-chart">
@@ -82,93 +66,76 @@
             //         console.log(error);
             //     });
             // },
-            getVentas(){
+            getEstadistica(numero){
                 let me = this;
-                var url = me.Ruta.estadistica + '/getEVentas';
-                axios.get(url).then(function(response){
-                    me.ventas = response.data.ventas;
-                    //cargamos los datos del chart
-                    me.loadVentas();
-                })
-                .catch(function(error){
-                    console.log(error);
-                });
+                let url;
+                switch (numero) {
+                    case 1:
+                        url = me.Ruta.estadistica + '/getEVentas';
+                        axios.get(url).then(function(response){
+                            me.ventas = response.data.ventas;
+                            //cargamos los datos del chart
+                            me.mostrar(1);
+                        })
+                        .catch(function(error){
+                            console.log(error);
+                        });
+                        break;
+                }
             },
-            // loadIngresos(){
-            //     let me = this;
-            //     me.ingresos.map(function(x){
-            //         me.varMesIngreso.push(x.mes);
-            //         me.varTotalIngreso.push(x.total);
-            //     });
-            //     me.varIngreso = document.getElementById('ingresos').getContext('2d');
-
-            //     me.charIngreso = new Chart(me.varIngreso, {
-            //         type: 'bar',
-            //         data: {
-            //             labels: me.varMesIngreso,
-            //             datasets: [{
-            //                 label: 'Ingresos',
-            //                 data: me.varTotalIngreso,
-            //                 backgroundColor: [
-            //                     'rgba(255, 99, 132, 0.2)',
-            //                 ],
-            //                 borderColor: [
-            //                     'rgba(255, 99, 132, 0.2)',
-            //                 ],
-            //                 borderWidth: 1
-            //             }]
-            //         },
-            //         options: {
-            //             scales: {
-            //                 yAxes: [{
-            //                     ticks: {
-            //                         beginAtZero: true
-            //                     }
-            //                 }]
-            //             }
-            //         }
-            //     });
-            // },
-            loadVentas(){
+            mostrar(numero){
                 let me = this;
-                me.ventas.map(function(x){
-                    me.varMesVenta.push(x.mes);
-                    me.numVentas.push(x.num_ventas);
-                });
-                me.varVenta = document.getElementById('ventas').getContext('2d');
-
-                me.charVenta = new Chart(me.varVenta, {
-                    type: 'bar',
-                    data: {
-                        labels: me.varMesVenta,
-                        datasets: [{
-                            label: 'Ventas',
-                            data: me.numVentas,
-                            backgroundColor: [
-                                'rgba(54, 162, 235, 0.2)',
-                            ],
-                            borderColor: [
-                                'rgba(54, 162, 235, 0.2)',
-                            ],
-                            borderWidth: 1
-                        }]
-                    },
-                    options: {
-                        scales: {
-                            yAxes: [{
-                                ticks: {
-                                    beginAtZero: true
+                switch (numero) {
+                    case 1:
+                        me.ventas.map(function(x){
+                            me.varMesVenta.push(me.fix(1, x.mes));
+                            me.numVentas.push(x.num_ventas);
+                        });
+                        me.varVenta = document.getElementById('ventas').getContext('2d');
+                        me.charVenta = new Chart(me.varVenta, {
+                            type: 'bar',
+                            data: {
+                                labels: me.varMesVenta,
+                                datasets: [{
+                                    label: 'Ventas',
+                                    data: me.numVentas,
+                                    backgroundColor: [
+                                        'rgba(54, 162, 235, 0.2)',
+                                    ],
+                                    borderColor: [
+                                        'rgba(54, 162, 235, 0.2)',
+                                    ],
+                                    borderWidth: 1
+                                }]
+                            },
+                            options: {
+                                scales: {
+                                    yAxes: [{
+                                        ticks: {
+                                            beginAtZero: true
+                                        }
+                                    }]
                                 }
-                            }]
-                        }
-                    }
-                });
+                            }
+                        });
+                        break;
+                }
             },
+            fix(numero, data = ''){
+                let fixed = '';
+                switch (numero) {
+                    case 1:
+                        let meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Setiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+                        fixed = meses[data-1];
+                        break;
+                }
+                return fixed;
+            }
             
         },
         mounted(){
                 // this.getIngresos();
-                this.getVentas();
+                this.getEstadistica(1);
         }
     }
 </script>
