f8f1ffa0152e4deadf15e16bb348a85a684f096c
root <erick_stalyn@outlook.es>
2020-01-11 22:39:07 +0000

    Cambios realizados en el VPS

--- public/js/app.js
+++ public/js/app.js
@@ -97539,7 +97539,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\laragon\www\EC_ci\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /var/www/html/EC_ci/resources/js/app.js */"./resources/js/app.js");
 
 
 /***/ })
