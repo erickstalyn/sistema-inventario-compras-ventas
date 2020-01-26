f57cf1bd2e890258b2c5ae4a22ef935967e5f513
Stalyn <espacherrezpuyen@gmail.com>
2020-01-26 16:21:30 -0500

    Cambio en el diseño del login

--- resources/views/auth/contenido.blade.php
+++ resources/views/auth/contenido.blade.php
@@ -5,11 +5,11 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="Control de inventario">
+  <meta name="description" content="SILMAR - Control de inventario">
   <meta name="author" content="EasyCode">
-  <meta name="keyword" content="Control de inventario">
+  <meta name="keyword" content="SILMAR - Control de inventario">
 
-  <title>Control de inventarios</title>
+  <title>SILMAR - Control de inventarios</title>
   
   <link rel="stylesheet" href="css/style.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/simple-line-icons/2.4.1/css/simple-line-icons.css">
