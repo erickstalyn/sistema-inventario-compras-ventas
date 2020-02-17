dd87bdca2421a9638e5a3a61f75512bb9b95e395
JoseXD <jose_30_escorpio@hotmail.com>
2020-02-16 23:38:10 -0500

    ventas se supone terminadas

--- database/migrations/2020_01_25_232854_create_detalle_venta__a_i_trigger.php
+++ database/migrations/2020_01_25_232854_create_detalle_venta__a_i_trigger.php
@@ -21,9 +21,9 @@ class CreateDetalleVentaAITrigger extends Migration
             DECLARE modo CHAR(1);
             SELECT SUBSTRING(tipo, 2, 1) INTO modo FROM venta WHERE id = new.venta_id;
             IF ( modo = '2' ) THEN
-                UPDATE detalle_producto dproducto
-                SET dproducto.reservados = dproducto.reservados + new.cantidad
-                WHERE dproducto.id = new.detalle_producto_id;
+                UPDATE detalle_producto
+                SET reservados = reservados + new.cantidad
+                WHERE id = new.detalle_producto_id;
             END IF;
         END
         ");
