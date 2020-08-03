c6345950562a7f26e1d31acb0b6d64f9749af16b
JoseXD <jose_30_escorpio@hotmail.com>
2020-08-03 00:53:34 -0500

    la verga

--- database/migrations/2019_09_08_030412_create_subproducto_table.php
+++ database/migrations/2019_09_08_030412_create_subproducto_table.php
@@ -16,8 +16,8 @@ class CreateSubproductoTable extends Migration
             $table->mediumIncrements('id'); // Por ser mediuminteger y unsigned tiene como valor maximo 16'777,215
             $table->string('nombre', 500);  // Se obtendrá uniendo el nombre del superproducto, el tamaño y el color
             $table->string('caracteristicas', 300);
-            $table->string('code_unique', 20);  // Codigo cifrado en base 36 para diferenciar los subproductos. Schema: [var1|1]-[var1_value|2]-[var2|1]-[var2_value|2]
-            $table->char('codigo', 15); // Estructura: [fecha-hora|12]-[numero aleatorio|1]
+            $table->string('code_unique', 20);  // Codigo cifrado en base 36 para diferenciar los subproductos. Schema: [tipo_caracteristica_id|1]-[caracteristica_id|2]
+            $table->char('codigo', 15); // Estructura: [fecha-hora|15]-[numero aleatorio|1]
             $table->string('descripcion', 100)->nullable();
             $table->longText('caracteristicas_json');    // Aqui se guardaran las caracteristicas de este subproductos. Por ejemplo: tamaño, color
             $table->decimal('costo_produccion', 11, 2)->default(0);
